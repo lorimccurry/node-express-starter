9499e4713c75673ee6ba69973e24b667ee2f232b
Lori McCurry <lori.mccurry@gmail.com>
2022-04-07 10:23:54 -0400

    rename to utils

---
+++ src/utils/auth.ts
@@ -0,0 +1,41 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import { jwtSecret } from '../envs';
+import prisma from './prisma';
+import { AUTH } from './../constants/messages';
+
+interface Cookies {
+  ACCESS_TOKEN?: string;
+}
+
+interface JwtPayload {
+  id: number;
+}
+
+export const verifyToken = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const { ACCESS_TOKEN: token } = req.cookies as Cookies;
+  const {
+    ERROR: { NOT_AUTHORIZED, NOT_VALID_USER },
+  } = AUTH;
+
+  if (token) {
+    let user;
+    try {
+      const { id } = jwt.verify(token, jwtSecret) as JwtPayload;
+      user = await prisma.user.findUnique({
+        where: { id },
+      });
+      if (!user) {
+        throw new Error(NOT_VALID_USER);
+      }
+    } catch (error) {
+      res.status(401).json({ error: NOT_AUTHORIZED });
+      return;
+    }
+    return next();
+  }
+};
